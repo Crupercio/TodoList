4a80db17b4c26dfbfe93cc38c071fbd1216f53a6
Luis Cruz Sanchez <luis.f.cruzsanchez@outlook.com>
2024-11-01 21:44:29 -0700

    Updated Index.cshtml to include the full namespace

--- AspNetCoreTodo/obj/Debug/net8.0/AspNetCoreTodo.AssemblyInfo.cs
+++ AspNetCoreTodo/obj/Debug/net8.0/AspNetCoreTodo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65507469f91da69a362694695a16de3c8ecdc473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+593d325b11b57e3bdbca12c211f1873ff879871d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
