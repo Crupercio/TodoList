65507469f91da69a362694695a16de3c8ecdc473
Luis Cruz Sanchez <luis.f.cruzsanchez@outlook.com>
2024-11-01 20:57:15 -0700

    added Controller and Models

--- AspNetCoreTodo/obj/Debug/net8.0/AspNetCoreTodo.AssemblyInfo.cs
+++ AspNetCoreTodo/obj/Debug/net8.0/AspNetCoreTodo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39fdb69a4a0c31aaf4a54a84c2021394c71115dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
