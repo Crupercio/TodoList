a5e9d63f370493bf9cb5fecb342140bf2325f546
Luis Cruz Sanchez <luis.cruzsanchez@stmartin.edu>
2024-11-05 14:23:18 -0800

    added table and migration

--- AspNetCoreTodo/obj/Debug/net8.0/AspNetCoreTodo.AssemblyInfo.cs
+++ AspNetCoreTodo/obj/Debug/net8.0/AspNetCoreTodo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+401b58ebc91ac6805ea54940835010cfca356881")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df03552f7aba524cea145f6f84d62c664865b71")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
